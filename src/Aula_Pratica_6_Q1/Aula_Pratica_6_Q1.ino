e3f820d54bfe937615640b314ecdf76e78caaf95
samuelcaldas <samuel.caldas@gmail.com>
2024-02-15 05:07:52 -0300

    Initial commit

---
+++ src/Aula_Pratica_6_Q1/Aula_Pratica_6_Q1.ino
@@ -0,0 +1,49 @@
+/**
+ * @file Aula_Pratica_6_Q1.ino
+ * @brief Aula Prática 6 - Servo motor controlado por botão
+ * @version 1.0
+ * @date 2023-06-04
+ *
+ * A. O roteiro envolve o desenvolvimento de uma lógica em que dois botões controlem o giro de um servo motor, um aumente e o outro diminua o giro.
+ * B. Deve conter 180 níveis com intervalo de 15ms.
+ * C. Quando chegar ao máximo ou mínimo, o botão correspondente não deverá mais atuar no giro.
+ * D. Se chegou no máximo/mínimo, devera ficar assim ate que o botão "oposto" seja pressionado.
+ */
+
+#include <Servo.h> // Inclui a biblioteca do Servo
+
+Servo meuservo;     // Cria um objeto Servo para controlar o servo motor
+int servoPin = 9;   // Define o pino do servo motor
+int BT1 = 2;        // Define o pino do botão BT1
+int BT2 = 3;        // Define o pino do botão BT2
+int pos = 0;        // Variável para armazenar a posição do servo motor
+int intervalo = 15; // Define o intervalo de tempo entre cada grau do servo motor
+
+void setup()
+{
+  meuservo.attach(servoPin);  // Anexa o servo motor ao pino 9
+  meuservo.write(pos);        // Define a posição do servo motor como 0 graus
+  pinMode(BT1, INPUT_PULLUP); // Define o pino do botão BT1 como entrada com resistor de pull-up interno
+  pinMode(BT2, INPUT_PULLUP); // Define o pino do botão BT2 como entrada com resistor de pull-up interno
+}
+
+void loop()
+{
+  if ((digitalRead(BT1) == LOW) && pos < 180) // Se o botão BT1 for pressionado e a posição do servo motor for menor que 180 graus
+  {
+    for (pos = 0; pos <= 180; pos++) // Gira o servo motor de 0 a 180 graus
+    {
+      meuservo.write(pos);
+      delay(intervalo);
+    }
+  }
+
+  if ((digitalRead(BT2) == LOW) && pos > 0) // Se o botão BT2 for pressionado e a posição do servo motor for maior que 0 graus
+  {
+    for (pos = 180; pos >= 0; pos--) // Gira o servo motor de 180 a 0 graus
+    {
+      meuservo.write(pos);
+      delay(intervalo);
+    }
+  }
+}
---
+++ src/Aula_Pratica_6_Q2/Aula_Pratica_6_Q2.ino
@@ -0,0 +1,30 @@
+/**
+ * @file Aula_Pratica_6_Q2.ino
+ * @brief Aula Prática 6 - Servo motor controlado por potenciômetro
+ * @version 1.0
+ * @date 2023-06-04
+ *
+ * A. O roteiro envolve o desenvolvimento de uma lógica em que um potenciômetro controla o giro de um servo motor.
+ */
+
+#include <Servo.h> // Inclui a biblioteca do Servo
+
+Servo meuservo;     // Cria um objeto Servo para controlar o servo motor
+int servoPin = 9;   // Define o pino do servo motor
+int potpin = A0;    // Define o pino do potenciômetro
+int val = 0;        // Variável para armazenar o valor lido do potenciômetro
+int intervalo = 15; // Define o intervalo de tempo entre cada grau do servo motor
+
+void setup()
+{
+  meuservo.attach(servoPin); // Anexa o servo motor ao pino 9
+  meuservo.write(val);       // Define a posição do servo motor como 0 graus
+}
+
+void loop()
+{
+  val = analogRead(potpin);        // Lê o valor do potenciômetro
+  val = map(val, 0, 1023, 0, 180); // Mapeia o valor para o intervalo de 0 a 180 graus
+  meuservo.write(val);             // Define a posição do servo motor com base no valor mapeado
+  delay(intervalo);                // Espera por 15 ms
+}
